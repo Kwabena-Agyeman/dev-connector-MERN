965761ee7c2d3909dac56c1f891d17f1f4a6fbfb
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-22 11:14:39 +0000

    added the route to add github user's repos and information

--- routes/api/Profile/index.js
+++ routes/api/Profile/index.js
@@ -1,6 +1,8 @@
 /** @format */
 
 const express = require("express");
+const request = require("request");
+const config = require("config");
 
 const router = express.Router();
 const authMiddleware = require("../../../middlewares/auth");
@@ -10,6 +12,8 @@ const profileModel = require("../../../models/Profile");
 const UserModel = require("../../../models/User");
 
 const validations = require("./validations");
+const { response } = require("express");
+const { body } = require("express-validator");
 
 // ****************************************************************************************************************//
 
@@ -335,6 +339,40 @@ router.delete("/education/:edu_id", authMiddleware, async (req, res) => {
   } catch (error) {
     console.error(error.message);
     res.status(500).send("server error");
+  }
+});
+
+//*************************************************************************************************************************** */
+// @route         GET  api/profile/github/:username
+// @description   GET USERS REPO FROM GITHUB
+// @access        PUBLIC Route
+
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created: asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}  `,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
   }
 });
 
