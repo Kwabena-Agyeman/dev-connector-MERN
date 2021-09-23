88ac4a328591ecda50a2c584e6feb6c2d956fb22
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-23 22:04:42 +0000

    I setup Redux and React-Redux

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Login from "./components/Auth/Login";
@@ -11,18 +15,20 @@ import "./App.css";
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </>
+      </Router>
+    </Provider>
   );
 };
 
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+/** @format */
+
+import { combineReducers } from "redux";
+
+export default combineReducers({});
