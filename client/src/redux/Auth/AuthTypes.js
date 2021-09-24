5f9bada39ccb59ae9a478d3f23ab2f169f0b0d21
Kwabena Agyeman <kagyeman@ymail.com>
2021-09-24 16:12:56 +0000

    set up default x-auth-token headers function

--- client/src/redux/Auth/AuthTypes.js
+++ client/src/redux/Auth/AuthTypes.js
@@ -2,3 +2,5 @@
 
 export const REGISTER_SUCCESS = "REGISTER_SUCESS";
 export const REGISTER_FAIL = "REGISTER_FAIL";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
